57f9544c9882a4f746cb00b7e6dc40f9c218b8a3
Adam Cox <adam.cox@ceridian.com>
2023-02-17 13:23:53 -0500

    first having issue with EffectsModule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { routing } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { ContactEffects } from './contact/contact-list/store/contact.effects';
+import { contactReducers } from './contact/contact-list/store/contact.reducer';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../material.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -28,6 +33,8 @@ import { OrderListComponent } from './order-list/order-list.component';
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
+    //EffectsModule.forRoot([ContactEffects]), // todo: resolve: Error: NG0203: inject() must be called from an injection context such as a constructor, a factory function, a field initializer, or a function used with `EnvironmentInjector#runInContext`
+    StoreModule.forRoot({ contacts: contactReducers }),
   ],
   providers: [],
   bootstrap: [AppComponent]
